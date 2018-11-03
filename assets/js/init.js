9c2677602ddab6625f41ba904f777c8392edb8a1
LiCybora <licybora@gmail.com>
2018-11-04 00:40:36 +0800

    delete unused file

--- assets/js/init.js
+++ assets/js/init.js
@@ -72,6 +72,10 @@ let setLocale = (lang) => {
 };
 
 $(function () {
+    if (/Edge\/\d./i.test(navigator.userAgent) || /Trident\/\d./i.test(navigator.userAgent)){
+        // This is Microsoft Edge
+        window.alert('Microsoft Edge or IE is not currently supported! Please use Firefox or Chromium and come back again!');
+    }
     $.ajaxSetup({
         async: false
     });

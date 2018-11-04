7f21a7fb0b236dcd4aaa8e169c44f19291f68a98
LiCybora <licybora@gmail.com>
2018-11-05 02:05:16 +0800

    Opps...should be good now

--- assets/js/init.js
+++ assets/js/init.js
@@ -148,7 +148,7 @@ $(function () {
             <span class="glyphicon glyphicon-remove"></span> <span data-v="Close" class="UI">${loadLocaleGeneral('Close', 'UI')}</span>
         </button>`
     );
-    $('#filterLabel').html(`<h2 class="modal-title UI" data-v="Filter" id="filterLabel">${loadLocaleGeneral('Filter', 'UI')}</h2>`);
+    $('#filterHead').html(`<h2 class="modal-title UI" data-v="Filter" id="filterLabel">${loadLocaleGeneral('Filter', 'UI')}</h2>`);
     $('#detailBtn').html(`
         <button type="button" class="btn btn-danger" data-dismiss="modal">
             <span class="glyphicon glyphicon-remove"></span> <span data-v="Close" class="UI">${loadLocaleGeneral('Close', 'UI')}</span>

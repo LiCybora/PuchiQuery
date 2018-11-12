a5feec58a7d19094716edd59e51d93fd67bfc3c1
Cybora <licybora@gmail.com>
2018-11-12 15:05:06 +0800

    Another attempt

--- assets/js/init.js
+++ assets/js/init.js
@@ -168,31 +168,4 @@ $(function () {
     `);
     $(this).attr('title', $localHead.text());
 
-    let previousScrollY = 0;
-
-    $(document).on('show.bs.modal', () => {
-        previousScrollY = window.scrollY;
-        $('html').addClass('modal-open').css({
-            marginTop: -previousScrollY,
-            overflow: 'hidden',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            position: 'fixed',
-        });
-    }).on('hidden.bs.modal', () => {
-        $('html').removeClass('modal-open').css({
-            marginTop: 0,
-            overflow: 'visible',
-            left: 'auto',
-            right: 'auto',
-            top: 'auto',
-            bottom: 'auto',
-            position: 'static',
-        });
-        window.scrollTo(0, previousScrollY);
-    });
-    
-
 });
